17ee2a8e85a2d7bb253308565ae124d0d496d350
karencita99 <86029294+karencita99@users.noreply.github.com>
2021-11-09 19:57:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,145 @@
+const listaDeproductosAgregadosAlCarrito = document.querySelector('#productosAgregadosAlCarrito'), tituloDeCarrito = document.querySelector('#tituloDeCarrito'), ElementosDelcarrito = document.querySelector('#carrito').childNodes, total = document.querySelector('#total'), PantallaDeCompraRealizada = document.querySelector('#CompraRealizada')
+ 
+
+let numeroDeProducto = 0, primeraVez = true
+
+window.onresize = ()=>{
+    if(window.outerWidth > 900){
+        const carrito = document.querySelector('nav')
+        carrito.style = ''
+    }
+}
+
+function agregarProductoAlCarrito(id) {
+    let producto = document.querySelector('#' + id).childNodes[1].childNodes;
+    let [nombre, descripcion, precio] = [producto[1].textContent, (producto[7].value) ? producto[7].value : '', producto[9].childNodes[1].textContent.split('').slice(9, 12).join('').replace(' ', '')]
+    let nuevoProductoAgregadoAlCarrito = document.createElement('div');
+    nuevoProductoAgregadoAlCarrito.classList.add('productoAgregadoAlCarrito')
+    nuevoProductoAgregadoAlCarrito.id = numeroDeProducto
+    nuevoProductoAgregadoAlCarrito.innerHTML = "<div><h2>" + nombre + "</h2><h3>" + descripcion + "</h3><h2>Precio: $" + precio + " MXN</h2></div><button onclick=\"eliminarProductoDelCarrito(" + numeroDeProducto + ")\"><svg width=\"20\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M17.9857 0L10 7.98571L2.01429 0L0 2.01429L7.98571 10L0 17.9857L2.01429 20L10 12.0143L17.9857 20L20 17.9857L12.0143 10L20 2.01429L17.9857 0Z\" fill=\"#fff\" fill-opacity=\"0.75\" /></svg></button>"
+    listaDeproductosAgregadosAlCarrito.appendChild(nuevoProductoAgregadoAlCarrito)
+    ObtenerTotal(precio, false)
+    numeroDeProducto++
+    if (primeraVez) {
+        //PrimerAnimacionDelCarrito()
+        PrimerProductoAgregado()
+        primeraVez = false
+    }
+    else {
+        animacionDelTotal()
+    }
+}
+
+function eliminarProductoDelCarrito(numero) {
+    let productoAEliminar = document.getElementById(numero)
+    const precio = productoAEliminar.childNodes[0].childNodes[2].textContent.split('').slice(9, 12).join('').replace(' ', '')
+    listaDeproductosAgregadosAlCarrito.removeChild(productoAEliminar)
+    ObtenerTotal(precio, true)
+    if (!listaDeproductosAgregadosAlCarrito.childElementCount) {
+        //ReiniciarCarritoAEstadoInicial()
+        ReiniciadoDeCarrito()
+        primeraVez = true
+    }
+}
+
+function RealizarCompra() {
+    const totalFinal = document.querySelector('#totalFinal'), direccionFinal = document.querySelector('#direccionFinal'), direccionIngresada = document.querySelector('#direccion'), advertencia = document.querySelector('#advertencia')
+    if(direccionIngresada.value !== ''){
+        totalFinal.textContent = total.textContent
+        direccionFinal.textContent = direccionIngresada.value
+        PantallaDeCompraRealizada.style= ''
+        animacionDePantallaDeCompraRealizada()
+        advertencia.classList.add('none')
+    }
+    else{
+        advertencia.classList.remove('none')
+    }
+}
+
+function cerrarCuadroDeCompraFinalizada (){
+    PantallaDeCompraRealizada.style = 'display: none;'
+}
+
+function AbrirElCarrito (){
+    const nav = document.querySelector('nav')
+    const botonDeAbrirCarrito = document.querySelector('#iconoDelCarrito')
+    if((nav.style.display === '') || (nav.style.display === 'none')){
+        nav.style.display= 'block'
+        nav.style.animation = 'aparecerdesdeabajo 0.3s linear'
+        botonDeAbrirCarrito.textContent = 'Seguir viendo productos'
+    }
+    else{
+        nav.style.animation= 'aparecerdesdeabajo 0.3s linear reverse'
+        botonDeAbrirCarrito.textContent = 'Ver mi carrito'
+    }
+    nav.addEventListener('animationend', ()=>{
+        if(nav.style.animation ==='0.3s linear 0s 1 reverse none running aparecerdesdeabajo'){
+            nav.style.display= 'none'
+        }
+        nav.style.animation = ''
+        nav.removeEventListener('animationend', ()=>{})
+    })
+}
+
+function ObtenerTotal(precioDeProductoRecienAgregado, restar) {
+
+    const obtenerTotal = parseInt(total.textContent.split('').splice(8, total.textContent.length).join('').replace(' ', ''))
+    if (restar) {
+        total.textContent = 'Total: $' + (obtenerTotal - parseInt(precioDeProductoRecienAgregado))
+    }
+    else {
+        total.textContent = 'Total: $' + (obtenerTotal + parseInt(precioDeProductoRecienAgregado))
+    }
+}
+
+function PrimerProductoAgregado() {
+    tituloDeCarrito.textContent = 'Lista de compras'
+    ElementosDelcarrito.forEach(elemento => {
+        if (elemento.classList) {
+            elemento.classList.remove('none')
+            animacionDeProductosDelCarritoInicial(elemento)
+        }
+    })
+}
+
+function ReiniciadoDeCarrito() {
+    tituloDeCarrito.textContent = 'Aquí te mostraremos tu lista de productos'
+    ElementosDelcarrito.forEach(elemento => {
+
+        if (elemento.classList) {
+            elemento.classList.add('none')
+            if (elemento.id === 'tituloDeCarrito') {
+                elemento.style = 'animation: aparecer 0.3s linear;'
+                elemento.classList.remove('none')
+                elemento.addEventListener('animationend', () => {
+                    elemento.style = ''
+                })
+            }
+        }
+    })
+}
+
+function animacionDelTotal() {
+    total.classList.add('animaciondestacar')
+    total.addEventListener('animationend', () => {
+        total.classList.remove('animaciondestacar')
+        total.classList.remove('animacionaparecerdesdelaizquierda')
+        total.removeEventListener('animationend', () => { })
+    })
+}
+
+function animacionDeProductosDelCarritoInicial(elemento) {
+    elemento.style = 'animation: aparecerdesdelaizquierda 0.3s linear;'
+    elemento.addEventListener('animationend', () => {
+        elemento.style = ''
+    })
+}
+
+function animacionDePantallaDeCompraRealizada(){
+    const contenedorDePantallaFinalAAnimar = PantallaDeCompraRealizada.childNodes[1]
+    contenedorDePantallaFinalAAnimar.style = 'animation: extender 0.2s linear;'
+    contenedorDePantallaFinalAAnimar.addEventListener('animationend', ()=>{
+        contenedorDePantallaFinalAAnimar.style= ''
+    })
+    
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Pagina de Venta de comida
+Pagina de venta de comida rapida comunmente consumida en mexico.
+Esta pagina esta hecha con:
+- HTML5
+- CSS3
+- Javascript vanilla
+Esta contiene un carrito y diseño responsive para moviles.
